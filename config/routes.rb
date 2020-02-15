45775f15236a6b903cd624ec37fc86e978738b05
Vincent <vincethewalker@yahoo.co.uk>
2020-02-15 16:32:46 +0000

    Added lots

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'todos/index'
+  get 'todos/new'
+  get 'todos/create'
+  get 'todos/show'
+  get 'todos/edit'
+  get 'todos/update'
+  get 'todos/destroy'
+  root 'pages#home'
+  get 'pages/home'
+  get 'pages/about'
+
+  resources :todos
 end
